5dea7459fea02dbbaaf6758243714f74d3d6f6fa
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-07-15 14:50:26 -0300

    For exercise

---
+++ Basics/Chapter 2/For/For/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace For
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)//carrega esta funçao junto do carregamento do form
+        {
+            int count; 
+            for (count = 2020; count >= 1950; count--) 
+            { 
+                cmbAno.Items.Add(count); 
+            }
+        }
+    }
+}
