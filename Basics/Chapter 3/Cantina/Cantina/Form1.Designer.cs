2626ac25cde5141701ec81866594dd726d7e3d53
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-07-16 15:30:25 -0300

    Canteen example

---
+++ Basics/Chapter 3/Cantina/Cantina/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Cantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.lstCaixa = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(130, 61);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 20);
+            this.txtCode.TabIndex = 0;
+            this.txtCode.TextChanged += new System.EventHandler(this.txtCode_TextChanged);
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.FormattingEnabled = true;
+            this.lstCaixa.Location = new System.Drawing.Point(318, 61);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(152, 225);
+            this.lstCaixa.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Código:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.AutoSize = true;
+            this.txtTotal.Location = new System.Drawing.Point(68, 194);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(0, 13);
+            this.txtTotal.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 355);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.txtCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.ListBox lstCaixa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtTotal;
+    }
+}
+
---
+++ Basics/Chapter 3/Cantina/Cantina/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantina
+{
+    public partial class Form1 : Form
+    {
+        string[] prod = new string[10]; 
+        string[] code = new string[10]; 
+        double[] value = new double[10]; 
+        double total;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carregarArray();
+            total = 0;
+        }
+
+        private void txtCode_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCode.Text.Length == 3)
+            {
+                for (int i = 0; i < prod.Length; i++)
+                {
+                    if (txtCode.Text == code[i])
+                    {
+                        lstCaixa.Items.Add(txtCode.Text + " - " + prod[i] + " - R$" + value[i]);
+                        total += value[i];
+                        txtTotal.Text = "Total: " + total;
+                        txtCode.Text = "";
+                        txtCode.Focus();
+                    }
+                }
+
+
+            }
+            else if (txtCode.Text.Length > 3)
+                MessageBox.Show("Código invalido");
+        }
+
+        private void carregarArray()
+        {
+            code[0] = "001";
+            code[1] = "002";
+            code[2] = "003";
+            code[3] = "004";
+            code[4] = "005"; 
+
+            prod[0] = "Pastel";
+            prod[1] = "Coxinha";
+            prod[2] = "Hot_Dog";
+            prod[3] = "Chocolate";
+            prod[4] = "Suco"; 
+
+            value[0] = 6.00;
+            value[1] = 5.00;
+            value[2] = 12.00;
+            value[3] = 3.50;
+            value[4] = 8.00;     
+        }
+
+    }
+}
