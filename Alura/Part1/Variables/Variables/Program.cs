107a265ce14d7374ba8a5c0a1921ffdc8090c80b
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-08-13 22:57:30 -0300

    2nd class

---
+++ Alura/Part1/Variables/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            Console.WriteLine("I'm 25");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Alura/Part1/floats/floats/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace floats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = 910.25;
+            Console.WriteLine(salary);
+            double num = 15 / 2.0; // one of the values must be in the float format so "num" can assume the same float format
+            Console.WriteLine(num);
+
+            int newSalary = Convert.ToInt32(salary);
+            Console.WriteLine(newSalary);
+
+            //long => 64bits integer
+            //short => 16bits integer
+
+            Console.ReadLine();
+        }
+    }
+}
