93ccc1227ef5d3c209aef072cdb2e62e0c497a0f
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-08-14 22:46:17 -0300

    Char/strings

---
+++ Alura/Part1/char/char/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'a';
+            Console.WriteLine(letter);
+
+            string phrase = "roi. leticia, ne?";
+            Console.WriteLine(phrase);
+
+            string phr = 
+@"-um
+-dois
+-tres";
+            Console.WriteLine(phr);
+
+            Console.ReadLine();
+        }
+    }
+}
