644a90c3e31e851a71a216080d65ea30f2bf33cd
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-08-16 17:28:48 -0300

    loop class

---
+++ Alura/Part1/while/while/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 1000;
+            int mes = 1;
+            //for(int mes=1;mes<13;mes++)
+
+            while (mes < 13)
+            {
+                valor += valor * 0.0036;
+                Console.WriteLine("apos " + mes + " meses: "+valor);
+                mes++;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
