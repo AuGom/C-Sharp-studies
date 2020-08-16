e76afafbe750a2d2cd4a60f8604f0b051dc8fd81
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-08-16 15:53:17 -0300

    Conditional class

---
+++ Alura/Part1/if/if/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            int quant = 2;
+            if (age >= 18)
+            {
+                Console.WriteLine("maior");
+            }
+            else if(quant>=2)
+            {
+                Console.WriteLine("acompanhado");
+            }
+            else
+            {
+                Console.WriteLine("menor");
+            }
+            Console.ReadLine();
+        }
+    }
+}
