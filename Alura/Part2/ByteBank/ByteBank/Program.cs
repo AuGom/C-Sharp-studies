96039898346d4949da034a58deae57c912ab0667
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-08-17 18:41:46 -0300

    1st class - part2

---
+++ Alura/Part2/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+
+            conta1.agencia = 2345;
+            conta1.conta = 353621;
+            conta1.saldo = 234.56;
+            conta1.titular = "augusto";
+
+
+        }
+    }
+}
