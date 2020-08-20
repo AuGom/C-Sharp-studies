2d2df7eebc83091b9cd6a841f66111c05ab39c88
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-08-20 18:21:21 -0300

    3rd class - part 2

--- Alura/Part2/ByteBank/ByteBank/ContaCorrente.cs
+++ Alura/Part2/ByteBank/ByteBank/ContaCorrente.cs
@@ -12,5 +12,42 @@ namespace ByteBank
         public int agencia;
         public int conta;
         public double saldo;
+
+        public void Sacar(double val)
+        {
+            if (this.saldo >= val)
+            {
+                this.saldo -= val;
+                Console.WriteLine("Operacao concluida");
+            }
+                
+            else
+                Console.WriteLine("Saldo insuficiente");
+
+        }
+
+        public void Deposito(double val)
+        {
+
+            this.saldo += val;
+            Console.WriteLine("Operacao concluida");
+
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.saldo += valor;
+            }
+        }
     }
+
+    
 }
+
--- Alura/Part2/ByteBank/ByteBank/Program.cs
+++ Alura/Part2/ByteBank/ByteBank/Program.cs
@@ -11,11 +11,33 @@ namespace ByteBank
         static void Main(string[] args)
         {
             ContaCorrente conta1 = new ContaCorrente();
+            ContaCorrente conta2 = new ContaCorrente();
+
 
             conta1.agencia = 2345;
             conta1.conta = 353621;
             conta1.saldo = 234.56;
             conta1.titular = "augusto";
+
+            conta2.agencia = 2345;
+            conta2.conta = 353921;
+            conta2.saldo = 2876.9;
+            conta2.titular = "luis";
+
+            conta1.Sacar(100);
+            Console.WriteLine(conta1.saldo);
+
+            conta1.Deposito(150);
+            Console.WriteLine(conta1.saldo);
+
+
+            conta2.Transferir(230, conta1);
+            Console.WriteLine(conta1.saldo);
+            Console.WriteLine(conta2.saldo);
+
+
+            Console.ReadLine();
+
 
 
         }
